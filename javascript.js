3f8ee53202607a684521b793f77cf38ade3418fe
Pxtchvm <111462541+Pxtchvm@users.noreply.github.com>
2025-03-30 13:48:13 +0800

    Add button functions

--- javascript.js
+++ javascript.js
@@ -14,12 +14,6 @@ function divide(a, b) {
   return a / b;
 }
 
-let number;
-
-let operator;
-
-let anotherNumber;
-
 function operate(operator, a, b) {
   if (operator === "+") {
     return add(a, b);
@@ -31,3 +25,59 @@ function operate(operator, a, b) {
     return divide(a, b);
   }
 }
+
+let number;
+
+let operator; // should remake this because the calculator display isn't limited to just one operator operating ie. `1+2-3+4`
+
+let anotherNumber; // should remake this because the calculator display isn't limited to just two numbers being operated on ie. `1+2+3+4`
+
+const display = document.querySelector("#calculator-display");
+
+const resultDisplay = document.querySelector("#result-display");
+
+const clearButton = document.querySelector("#clear-btn");
+
+const backspaceButton = document.querySelector("#backspace-btn");
+
+const equalsButton = document.querySelector("#equals-btn");
+
+const funButton = document.querySelector("#fun-btn");
+
+const digitButtons = document.querySelectorAll(".digit-btn");
+
+const operatorButtons = document.querySelectorAll(".operator-btn");
+
+clearButton.addEventListener("click", () => {
+  number = undefined;
+  operator = undefined;
+  anotherNumber = undefined;
+  display.textContent = "";
+});
+
+backspaceButton.addEventListener("click", () => {
+  let text = display.textContent;
+  let textLength = display.textContent.length;
+  display.textContent = text.slice(0, textLength - 1);
+});
+
+funButton.addEventListener("click", () => {
+  alert("calculator is fun isn't it?!")
+});
+
+equalsButton.addEventListener("click", () => {
+  // implement equals functionality
+});
+
+digitButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    display.textContent += button.textContent;
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    number = +display.textContent;
+    display.textContent += button.textContent;
+  });
+});
